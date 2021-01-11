215018f6e67cb7fa0476506327c597f37c1491a4
JulyaMachado <contato@julyamachado.com>
2021-01-11 13:19:49 -0300

    Estilizando form

--- application/src/views/Login.vue
+++ application/src/views/Login.vue
@@ -1,10 +1,18 @@
 <template>
     <div class="login flex-container">
-        <form>
+        <form class="form-content">
             <h1 title="title-box-content">Log in</h1>
-            <input type="text" placeholder="username@email.com" />
-            <input type="password" placeholder="Password" />
-            <button type="submit">Go</button>
+            <div class="form-input">
+                <!--<label>Username</label><br />-->
+                <input type="text" placeholder="username@email.com" />
+            </div>
+            <div class="form-input">
+                <!--<label>Password</label><br />-->
+                <input type="password" placeholder="Password" />
+            </div>
+            <div class="form-button">
+                <button type="submit">Go</button>
+            </div>
         </form>
     </div>
 </template>
@@ -31,6 +39,10 @@ body{
     height: 100vh;
 }
 
+input, button{
+    outline: none!important;
+}
+
 .flex-container{
     display: flex;
     flex-direction: column;
@@ -48,5 +60,33 @@ body{
     border-radius: 20px;
     box-shadow: 1px 1px 15px rgba(0,0,0,.25);
 }
+
+.form-input label{
+    text-align: left;
+    position: relative;
+    float: left;
+    margin-top: 15px;
+    font-size: .8rem;
+    font-weight: 600;
+}
+
+.form-input input{
+    padding: 10px 15px;
+    border-radius: 30px;
+    border: 1px solid #999;
+}
+
+.form-input input:focus{
+    padding: 15px 20px;
+    transition: .4s;
+    -webkit-transition: .4s;
+    -moz-transition: .4s;
+    -o-transition: .4s;
+}
+
+.form-button button{
+    margin-top: 15px;
+}
+
 
 </style>
