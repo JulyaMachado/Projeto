570c31eb401cfe2c2c1f6655da4abcfa6f1aec02
JulyaMachado <contato@julyamachado.com>
2021-01-11 11:23:53 -0300

    Base form login

---
+++ application/src/views/Login.vue
@@ -0,0 +1,14 @@
+ <template>
+    <div class="login">
+        <h1 class="title-login-box">Log in</h1>
+        <input type="email" placeholder="username@email.com" />
+        <br />
+        <input type="password" placeholder="Password" />
+        <br />
+        <label><input type="checkbox" name="" /> Remember me</label>
+        <a href="#">Forgot</a>
+        <br />
+        <button type="submit">Go</button>
+        <span>Don't have an account? <a href="">Create.</a>
+    </div>
+ </template>
